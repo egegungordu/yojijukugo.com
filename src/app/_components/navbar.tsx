9e027f38c74435f32852eb0d772bb3fc62918305
Ege Gungordu <egegungorduu@hotmail.com>
2024-05-20 06:40:23 +0300

    feat: add logo, change text selection color

--- src/app/_components/navbar.tsx
+++ src/app/_components/navbar.tsx
@@ -13,11 +13,16 @@ export default function Navbar() {
   const path = usePathname();
 
   return (
-    <nav className="h-full shrink-0 bg-gradient-to-b from-black leading-none via-neutral-950 to-neutral-600 flex text-white [writing-mode:vertical-lr] py-2 items-center">
-      <Link href="/">
-        <h1 className="text-xl text-neutral-200 py-2 hover:text-white">
+    <nav className="h-full shrink-0 bg-gradient-to-b from-black leading-none via-neutral-950 to-neutral-600 flex text-white [writing-mode:vertical-lr] items-center border-r border-black">
+      <Link href="/" className="py-2 w-full flex items-center justify-center">
+        {/*<h1 className="text-xl text-neutral-200 py-2 hover:text-white">
           四字熟語.com
-        </h1>
+        </h1>*/}
+        <img
+          src="/logo.svg"
+          alt="四字熟語.com"
+          className="size-5 text-white mx-auto hover:scale-105 transition-transform"
+        />
       </Link>
 
       {LINKS.map(({ href, text }) => (
@@ -25,7 +30,7 @@ export default function Navbar() {
           key={href}
           href={href}
           className={cn(
-            "font-semibold bg-black text-white px-2 py-3 hover:bg-neutral-700 transition-colors",
+            "font-semibold bg-black text-white pr-2 py-3 hover:bg-neutral-700 transition-colors pl-[13px]",
             {
               "bg-neutral-50 text-black hover:bg-neutral-50": path === href,
             },
