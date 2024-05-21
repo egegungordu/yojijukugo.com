1d014f02b811548e0cd133c248ff7e0da7dbd247
Ege Gungordu <egegungorduu@hotmail.com>
2024-05-21 23:49:51 +0300

    feat: iterate design

--- src/app/(routes)/page.tsx
+++ src/app/(routes)/page.tsx
@@ -3,7 +3,7 @@ import YojijukugoCanvas from "./_components/yojijukugo-canvas";
 export default function Home() {
   return (
     <main className="isolate relative h-full w-full flex">
-      <div className="sm:border-r max-w-screen-sm w-full flex flex-col py-4">
+      <div className="sm:border-r max-w-screen-sm shrink-0 w-full flex flex-col py-4">
         <div className="font-medium text-center text-muted-foreground px-4 sm:px-8">
           今日の四字熟語
         </div>
@@ -45,11 +45,12 @@ export default function Home() {
           </p>
         </div>
 
-        <div className="flex items-center justify-center mt-auto text-center text-muted-foreground text-sm px-4 sm:px-8">
+        <div className="flex items-center justify-center mt-auto text-center text-muted-foreground text-sm px-4 sm:px-6 tracking-tighter">
           四字熟語（よじじゅくご）は、漢字4文字で構成される日本語の慣用句や成句のことを指します。
         </div>
       </div>
 
+      <div className="overflow-hidden h-full w-full">
       <YojijukugoCanvas
         style={{
           maskImage:
@@ -57,8 +58,9 @@ export default function Home() {
           WebkitMaskImage:
             "radial-gradient(circle at 0 0,white 50%, rgba(0,0,0,0) 100%)",
         }}
-        className="max-sm:hidden"
+        className="max-sm:hidden animate-in fade-in-0 duration-500"
       />
+      </div>
     </main>
   );
 }
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -60,7 +60,7 @@ export default function RootLayout({
       >
         <ThemeProvider
           attribute="class"
-          defaultTheme="light"
+          defaultTheme="dark"
           disableTransitionOnChange
         >
           <Navbar />
