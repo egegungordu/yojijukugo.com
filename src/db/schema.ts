9c6aaeab6686e2a0f1c152b1ad0af2b38d4c110b
Ege Gungordu <egegungorduu@hotmail.com>
2024-06-07 05:31:44 +0300

    misc: update word schema

--- src/db/schema.ts
+++ src/db/schema.ts
@@ -1,24 +1,26 @@
-import { timestamp, text, pgTableCreator } from "drizzle-orm/pg-core";
+import { timestamp, serial, text, pgTableCreator, uniqueIndex, integer } from "drizzle-orm/pg-core";
 import { randomUUID } from "crypto";
 import { relations } from "drizzle-orm";
 
 const pgTable = pgTableCreator((name) => `yojijukugo_${name}`);
 
 export const word = pgTable("word", {
-  id: text("id")
-    .primaryKey()
-    .$defaultFn(() => randomUUID()),
-  word: text("word").unique(),
+  id: serial("id").primaryKey(),
+  word: text("word"),
   reading: text("reading"),
   meaning: text("meaning"),
+  dictionary: text("dictionary"),
+  raw: text("raw"),
   createdAt: timestamp("createdAt", { mode: "date" }).notNull().defaultNow(),
-});
+}, (table) => ({
+  unique: uniqueIndex("word-dictionary-unique").on(table.word, table.dictionary),
+}));
 
 export const wotd = pgTable("wotd", {
   id: text("id")
     .primaryKey()
     .$defaultFn(() => randomUUID()),
-  wordId: text("wordId")
+  wordId: integer("wordId")
     .references(() => word.id)
     .notNull(),
   createdAt: timestamp("createdAt", { mode: "date" })
